45d9975e4faddd001a4bca8d77bbeaa15b985769
Gouravgilhare <gouravgilhare4@gmail.com>
2025-06-03 01:01:50 +0530

    CarBikeAutoAdded

--- frontEnd/src/components/LocationSearchPanel.jsx
+++ frontEnd/src/components/LocationSearchPanel.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 const LocationSearchPanel = () => {
-  return <div>Location</div>;
+  return (
+    <div>
+      <div className="flex gap-4 items-center my-4 justify-start">
+        <h2 className="bg-[#eee] rounded-full w-8 h-8 flex text-lg justify-center items-center">
+          <i className="ri-map-pin-2-fill "></i>
+        </h2>
+        <h4 className="text-sm bg-[#eee] px-2  font-medium rounded-full">
+          742 Evergreen Terrace Springfield, USA ZIP: 62704
+        </h4>
+      </div>
+      <div className="flex gap-4 items-center my-4 justify-start">
+        <h2 className="bg-[#eee] rounded-full w-8 h-8 flex text-lg justify-center items-center">
+          <i className="ri-map-pin-2-fill "></i>
+        </h2>
+        <h4 className="text-sm bg-[#eee] px-2  font-medium rounded-full">
+          10 Downing Street Westminster, London, UK Postal Code: SW1A 2AA
+        </h4>
+      </div>
+      <div className="flex gap-4 items-center my-4 justify-start">
+        <h2 className="bg-[#eee] rounded-full w-8 h-8 flex text-lg justify-center items-center">
+          <i className="ri-map-pin-2-fill "></i>
+        </h2>
+        <h4 className="text-sm bg-[#eee] px-2  font-medium rounded-full">
+          1600 Pennsylvania Avenue NW Washington, D.C., USA ZIP: 20500
+        </h4>
+      </div>
+      <div className="flex gap-4 items-center my-4 justify-start">
+        <h2 className="bg-[#eee] rounded-full w-8 h-8 flex text-lg justify-center items-center">
+          <i className="ri-map-pin-2-fill "></i>
+        </h2>
+        <h4 className="text-sm bg-[#eee] px-2  font-medium rounded-full">
+          1-1 Chiyoda Chiyoda City, Tokyo, Japan Postal Code: 100-8111
+        </h4>
+      </div>
+      <div className="flex gap-4 items-center my-4 justify-start">
+        <h2 className="bg-[#eee] rounded-full w-8 h-8 flex text-lg justify-center items-center">
+          <i className="ri-map-pin-2-fill "></i>
+        </h2>
+        <h4 className="text-sm bg-[#eee] px-2  font-medium rounded-full">
+          221B Baker Street London, UK Postal Code: NW1 6XE
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default LocationSearchPanel;
--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
 
   return (
     <>
-      <div className="h-screen absolute">
+      <div className="h-screen absolute overflow-hidden">
         <img
           src="/images/uberLogo.png"
           alt="uberlogo"
@@ -91,6 +91,60 @@ const Home = () => {
           </div>
           <div ref={panelRef} className=" bg-white  h-0 ">
             <LocationSearchPanel />
+          </div>
+        </div>
+
+        <div className="fixed w-full  z-10 p-2 bg-white bottom-0">
+          <h3 className="text-2xl font-semibold p-4 ">Choose a Vehicle</h3>
+          <div className="flex w-full p-3 py-6 border-2 border-gray-100 active:border-black mb-2 bg-gray-100   rounded-xl items-center justify-between">
+            <img className=" h-12" src="/images/car.png" alt="car" />
+            <div className="  w-1/2">
+              <h4 className="font-medium text-base">
+                UberGO{" "}
+                <span>
+                  {" "}
+                  <i className="ri-user-3-fill "></i>
+                </span>
+                4
+              </h4>
+              <h5 className="font-medium text-base">2 Mins away</h5>
+              <p className="font-normal text-xs">Affordable, compact rides</p>
+            </div>
+            <h2 className="text-lg font-semibold">₹195.50</h2>
+          </div>
+          <div className="flex w-full p-3 py-6 border-2 mb-2 bg-gray-100 border-gray-100 active:border-black  rounded-xl items-center justify-between">
+            <img className=" h-12" src="/images/auto.png" alt="car" />
+            <div className="  w-1/2">
+              <h4 className="font-medium text-base">
+                UberAuto{" "}
+                <span>
+                  {" "}
+                  <i className="ri-user-3-fill "></i>
+                </span>
+                3
+              </h4>
+              <h5 className="font-medium text-base">3 Mins away</h5>
+              <p className="font-normal text-xs">Affordable, auto rides</p>
+            </div>
+            <h2 className="text-lg font-semibold">₹118.50</h2>
+          </div>
+          <div className="flex w-full p-3 py-6 border-2 mb-2 bg-gray-100 border-gray-100 active:border-black  rounded-xl items-center justify-between">
+            <img className=" h-12" src="/images/bike.png" alt="car" />
+            <div className="  w-1/2">
+              <h4 className="font-medium text-base">
+                Moto{" "}
+                <span>
+                  {" "}
+                  <i className="ri-user-3-fill "></i>
+                </span>
+                1
+              </h4>
+              <h5 className="font-medium text-base">2 Mins away</h5>
+              <p className="font-normal text-xs">
+                Affordable, motorcycle rides
+              </p>
+            </div>
+            <h2 className="text-lg font-semibold">₹65</h2>
           </div>
         </div>
       </div>
